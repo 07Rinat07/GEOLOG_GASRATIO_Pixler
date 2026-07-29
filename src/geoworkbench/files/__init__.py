1f88bc4b039a45906c743f5688113159d1ca3d2c
S.Rinat <102806560+07Rinat07@users.noreply.github.com>
2026-07-29 17:51:04 +0500

    Export Files workspace services

--- src/geoworkbench/files/__init__.py
+++ src/geoworkbench/files/__init__.py
@@ -6,16 +6,31 @@ from geoworkbench.files.archive_service import (
     ArchiveFormat,
     ArchiveService,
 )
+from geoworkbench.files.datum import (
+    DatumCalculationError,
+    DatumElevations,
+    calculate_datum_elevations,
+)
 from geoworkbench.files.document_service import DocumentKind, DocumentService
 from geoworkbench.files.engineering import EngineeringCalculator, UnitConverter
+from geoworkbench.files.logo_service import LogoDesign, LogoDesignError, LogoService
+from geoworkbench.files.pdf_tools import PdfTools, PdfToolsError
 
 __all__ = [
     "ArchiveCapability",
     "ArchiveEntry",
     "ArchiveFormat",
     "ArchiveService",
+    "DatumCalculationError",
+    "DatumElevations",
     "DocumentKind",
     "DocumentService",
     "EngineeringCalculator",
+    "LogoDesign",
+    "LogoDesignError",
+    "LogoService",
+    "PdfTools",
+    "PdfToolsError",
     "UnitConverter",
+    "calculate_datum_elevations",
 ]
